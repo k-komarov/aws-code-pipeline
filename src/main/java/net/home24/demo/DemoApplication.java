db34b563c1ca87d00297c6067b4621349ff5d6f8
Kirill Komarov <kirill.komarov@home24.de>
2018-05-07 11:12:29 +0200

    Springboot docker demo

---
+++ src/main/java/net/home24/demo/DemoApplication.java
@@ -0,0 +1,20 @@
+package net.home24.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @GetMapping("/ping")
+    public String ping() {
+        return "pong";
+    }
+}
